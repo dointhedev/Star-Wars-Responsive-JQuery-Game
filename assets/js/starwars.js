bc73e41c12ef6172a53b33c217931748e7a93bab
Avi Davis Mac <oodavis.avi@gmail.com>
2018-09-12 01:43:33 -0700

    refractored the JS. Need to finish the new round function an clean up the code.

--- assets/js/starwars.js
+++ assets/js/starwars.js
@@ -1,59 +1,8 @@
 // JavaScript Document
 // Document load start JS process 
-$(document).ready(function () {
-    // I run all my functions out of this function. 
-    startScreen();
-});
+$(document).ready(startScreen);
 
-/*::::::DOM CACHE::::::*/
-
-
-/*::::::GLOBALS::::::*/
-
-// Fighter stats
-var fighterObj = {
-    obiWan: {
-        health: 100,
-        power: 120,
-        counter: 100,
-    },
-    lukeSkywalker: {
-        health: 100,
-        power: 150,
-        counter: 100,
-    },
-    darthSidious: {
-        health: 100,
-        power: 180,
-        counter: 100,
-    },
-    darthVader: {
-        health: 100,
-        power: 150,
-        counter: 100,
-    },
-
-};
-
-// all my image assets stored in one variable
-var fighterImages = "<div class='images py-4  d-flex flex-sm-row flex-column'>" +
-    "<div class='lukeSkywalker col-sm-3 fightImages '>" + "<img id='lukeSkywalker' class='img-fluid fighter reg_border px-2 py-1 mr-1' src='assets/images/lukeSkywalker.png' alt=''>" + "<span><p><b>Power:</b>" + " " + fighterObj.lukeSkywalker.power + "</p></span>" + "</div>" +
-    "<div class='obiWan  col-sm-3 fightImages'>" + "<img id='obiWan' class='img-fluid fighter reg_border px-2 py-1 mr-1' src='assets/images/obiWan.png' alt=''>" + "<span><p><b>Power:</b>" + " " + fighterObj.obiWan.power + "</p></span>" + "</div>" +
-    "<div class='darthSidious  col-sm-3 fightImages'>" + "<img id='darthSidious' class='img-fluid fighter reg_border px-2 py-1 mr-1' src='assets/images/darthSidious.png' alt=''>" + "<span><p><b>Power:</b>" + " " + fighterObj.darthSidious.power + "</p></span>" + "</div>" +
-    "<div class='darthVader  col-sm-3 fightImages'>" + "<img id='darthVader' class='img-fluid fighter reg_border px-2 py-1 mr-1' src='assets/images/darthVader.png' alt=''>" + "<span><p><b>Power:</b>" + " " + fighterObj.darthVader.power + "</p></span>" + "</div>" +
-    "</div>";
-
-// Dynamic fighter container 
-var fightCont;
-
-// I use this later to grab the ID of the the onlclicked element and use that in various different ways. 
-var id;
-
-// Global variables for health;
-var fighterHealth = 100;
-var enemyHealth = 100;
-
-
+/*::::::DATA SETS::::::*/
 
 // This is where I add the active player stats that has been chose by user. 
 var activeObj = {};
@@ -62,135 +11,201 @@ var enemyObj = {};
 // Object to push defeated fighter too. 
 var defeatedObj = {};
 
+// Fighter stats.
+const fighterObj = {
+    obiWan: {
+        health: 100,
+        power: 120,
+        counter: 100,
+        img: 'assets/images/obiWan.png'
+    },
+    lukeSkywalker: {
+        health: 100,
+        power: 150,
+        counter: 100,
+        img: 'assets/images/lukeSkywalker.png'
+    },
+    darthSidious: {
+        health: 100,
+        power: 180,
+        counter: 100,
+        img: 'assets/images/darthSidious.png'
+    },
+    darthVader: {
+        health: 100,
+        power: 150,
+        counter: 100,
+        img: 'assets/images/darthVader.png'
+    },
+
+};
+
+function genElement(type, className, id) {
+    const el = $(type).addClass(className);
+    if (id !== null) {
+        el.attr("id", id);
+    }
+    return el;
+}
+
+function genStats(fighter) {
+    const statCont = genElement("<div>", "player-states", null);
+    const statUl = genElement("<ul>", "stat-list list-group", null);
+    const pwrLi = genElement("<li>", "stat-list-item list-group-item", null).text(`Power: ${fighterObj[fighter].power}`)
+    const cntrLi = genElement("<li>", "stat-list-item list-group-item", null).text(`Counter: ${fighterObj[fighter].counter}`)
+    statUl.append(pwrLi, cntrLi)
+    return statCont.append(statUl)
+}
+
+function genCard(fighter, obj, num) {
+    const card = genElement("<div>", "card d-flex flex-row", `card-${fighter}`)
+    const row = genElement("<div>", "row no-gutters", null)
+    const colAuto = genElement("<div>", "col-auto", null)
+    const ftrImg = genElement("<img>", "img-fluid mx-2 mb-2", null).attr("src", `assets/images/${fighter}.png`)
+    const col = genElement("<div>", "col", null)
+    const crdBlk = genElement("<div>", "card-block px-2", null)
+    const crdTle = genElement("<h2>", "card-title mt-3 ml-2", null).text(fighter)
+    const crdTxt = genElement("<div>", "card-block px-2", null).html(genStats(fighter));
+    const prgTle = genElement("<h4>", "mt-3 ml-2", null).text("Health")
+    const hlth = genElement("<div>", "progress", null)
+    const prgsBR = genElement("<div>", "progress-bar bg-success", `hlthBr-${num}`).attr("aria-valuenow", fighterObj[fighter].health).attr("role", "progressbar").attr("style", ` width: ${fighterObj[fighter].health}%;`).text(`${fighterObj[fighter].health}%`)
+    colAuto.append(ftrImg)
+    hlth.append(prgsBR)
+    col.append(crdBlk, crdTle, crdTxt, prgTle, hlth)
+    return card.append(row.append(colAuto).append(col))
+}
+
+
+function genFighters() {
+    const imgCont = genElement("<div>", 'images py-4  d-flex flex-sm-row flex-column pt-4 mt-4', null);
+
+    for (let el in fighterObj) {
+        const imgDiv = genElement("<di>", `${el} col-sm-3 fightImages text-center mt-2`, null);
+        const fImg = genElement("<img>", 'img-fluid fighter reg_border px-2 py-1 mt-1', el).attr("src", fighterObj[el].img);
+        const span = genElement("<div>", `fightStats text-center text-white d-block py-2`, null).html(`<b>Power:</b> ${fighterObj[el].power}`)
+        imgDiv.append(fImg, span)
+        imgCont.append(imgDiv)
+    }
+    return imgCont;
+}
+
+
+
+
 function startScreen() {
     fightCont = $("#fighters");
-    fightCont.html("<h3 class='remove-title'>Choose a Fighter</h3>" + fighterImages);
-    gameSetup();
-}
-
-function startFight() {
-    // onclick function to move the images from one staging area to the next and to exchange data between the obj's 
-
-    $(".fighter").click(function () {
-        fightCont = $("#fighters");
-        id = $(this).attr("id");
-        activeObj = fighterObj[id];
-        delete fighterObj[id];
-        fighterPower = activeObj.power;
-        $("." + id).addClass("d-none");
-        $(".fightImages").removeClass("col-sm-3").addClass("col-sm");
-        $(".remove-title").empty();
-        $(".remove-title").append("<h3>Your Enemies</h3>");
-       
-        $("#fighterActive").html("<h3>Your Character</h3>" + "<img id='" + id + "' src='assets/images/" + id + ".png'" + "'" + "data-active='false'" + "class='img-fluid d-block activeFighter reg_border px-2 py-1 mr-1'>" + "<span id='playerStats' class='bg-success'><p><b>Power:</b>" + " " + activeObj.power + "</p></span>");
-        $("#fightCont").replaceWith("");
-        $("#EATA").append("<div id='enemies' class='col-12 d-block py-2'>" + $(fightCont).html() + "</div>");
-        $("#fighters").html("");
-        $("#fightCont").replaceWith("");
-
-
-
-        $(".fighter").click(function () {
-
-            id = $(this).attr("id");
-            var enemyCont = $("#attackEnemies");
-            enemyObj = fighterObj[id];
-            enemyPower = enemyObj.power;
-            console.log();
-            // test to see if enemy power is set
-            console.log(enemyObj.power);
-            delete fighterObj[id];
-
-            $("#enemyActive").html(" <h3>Your Enemy</h3> <img id='" + id + "' src='assets/images/" + id + ".png'" + "class='img-fluid d-block activeEnemy reg_border px-2 py-1 mr-1'>" + "<span id='playerStats' class='bg-success'><p><b>Power:</b>" + " " + enemyObj.power + "</p></span>");
-            $("." + id).addClass("d-none");
-            $("#attack-btn").on("click", attack);
-
-            function scoreBoard() {
-                console.log(fighterPower);
-                console.log(enemyPower);
-                $("#scoreBoard").toggle("fast");
-                $("#fighterHAlert").html("<strong>Fighters Health:</strong>" + " " + fighterHealth + "%");
-                $("#enemyHAlert").html("<strong>Enemies Health:</strong>" + " " + enemyHealth + "%");
-            }
-
-
-
-            function attack() {
-                // Local Variables for the attack function 
-                var fighterPower = activeObj.power; // activeObj.power;
-                var enemyPower = enemyObj.power;
-                var fighterNumb = Math.floor(Math.random() * fighterPower);
-                var enemyNumb = Math.floor(Math.random() * enemyPower);
-
-
-                // loads the top scoreboard
-                scoreBoard();
-                $("#scoreBoard").removeClass("d-none").addClass("d-block sticky-top");
-                $("#attack-btn").text("Attack");
-
-                // Animation complete.
-
-
-                if ((fighterNumb >= enemyNumb) && ((fighterHealth > 0) && (enemyHealth > 0))) {
-                    console.log("Fighter Won");
-                    enemyHealth = parseInt(enemyHealth) - 10;
-                    // enemyObj[id] = fighterObj[id];
-
-                } else if ((fighterNumb <= enemyNumb) && ((fighterHealth > 0) && (enemyHealth > 0))) {
-                    console.log("Fighter Loss");
-
-                    fighterHealth = parseInt(fighterHealth) - 10;
-                    //console.log(defeatedFighterObj);
-
-                } else {
-                    if ((fighterHealth > enemyHealth)) {
-                        $.extend(defeatedObj, defeatedObj);
-
-                        delete enemyObj.power;
-                        delete enemyObj.health;
-                        delete enemyObj.counter;
-                        // Test your Objects with the following code 
-                        var strA = JSON.stringify(activeObj, null, 4); // this turns the object into a string and makes it easy to read in the console.
-                        console.log("active:" + strA);
-
-                        var strB = JSON.stringify(fighterObj, null, 4);
-                        console.log("fighter:" + strB);
-
-                        var strC = JSON.stringify(enemyObj, null, 4);
-                        console.log("enemy:" + strC);
-
-                        var strD = JSON.stringify(defeatedObj, null, 4);
-                        console.log("defeated:" + strD);
-
-
-                        console.log("Fighter WIIIINNNS");
-                        $("#enemyHAlert").html("");
-                        $("#fighterHAlert").html("<strong>You won with:</strong>" + " " + fighterHealth + "%" + " left");
-                        var delayNextRound = setTimeout(function () {
-                            $("#scoreBoard").removeClass("d-block").addClass("d-none");
-
-
-                            nextRound();
-
-                        }, 5000);
-
-
-                    } else
-                        console.log("Fighter LOSSES");
-                    $("#fighterHAlert").html("");
-                    $("#enemyHAlert").html("<strong>You lost with:</strong>" + " " + fighterHealth + "%" + " left");
-                    var delayEndGame = setTimeout(function () {
-                        $("#scoreBoard").removeClass("d-block").addClass("d-none");
-                        endGame();
-
-                    }, 5000);
-                }
-            }
-
-        });
-    });
+    const h3 = genElement("<h3>", 'remove-title text-center', null).text("Choose a Fighter");
+    const fighterImages = genFighters();
+    //  const btn = genElement("<button>", 'btn btn-outline-light', 'attack-btn').text("Start The Game");
+    fightCont.append(h3, fighterImages);
+    $(".fighter").click(chsPlr);
 
 }
+
+
+function chsPlr() {
+    const atkBtn = $("#attack");
+    const ftrId = $(this).attr("id");
+    const actvFtr = $("#fighterActive");
+    const actvEnmy = $("#enemyActive");
+    const btn = genElement("<button>", 'btn btn-danger', 'attack-btn').text("ATTACK!!");
+
+    $("." + ftrId).addClass("d-none");
+
+    if (jQuery.isEmptyObject(activeObj)) {
+        activeObj[ftrId] = fighterObj[ftrId];
+        $(".remove-title").text("Choose an Enemy");
+        actvFtr.html(genCard(ftrId, fighterObj[ftrId], 0)).prepend("<strong>Your Fighter:</strong>");
+    } else {
+        enemyObj[ftrId] = fighterObj[ftrId];
+        $(".remove-title").text("Remaining Enemy Queue").addClass("mt-5");
+        actvEnmy.html(genCard(ftrId, fighterObj[ftrId], 1)).prepend("<strong>Your Enemy:</strong>");
+        atkBtn.html(btn);
+        $("#attack-btn").click(attack);
+    }
+}
+
+function attack() {
+    // Local Variables for the attack function 
+    const aObj = activeObj[Object.keys(activeObj)[0]];
+    const eObj = enemyObj[Object.keys(enemyObj)[0]];
+    const fighterPower = aObj.power; // activeObj.power;
+    let fighterHealth = aObj.health; // activeObj.health;
+
+    const enemyPower = eObj.power;
+    let enemyHealth = eObj.health;
+
+
+    const fighterNumb = Math.floor(Math.random() * fighterPower);
+    const enemyNumb = Math.floor(Math.random() * enemyPower);
+
+    // Animation complete.
+    if ((fighterHealth > 0) && (enemyHealth > 0)) {
+        if (fighterNumb >= enemyNumb) {
+            console.log("Fighter Won");
+            aObj.health = aObj.health - 10;
+            $('#hlthBr-0').attr("style", `width: ${aObj.health}%`).attr("aria-valuenow", aObj.health).text(`${aObj.health}%`)
+        } else {
+            console.log("Fighter Loss");
+            eObj.health = enemyHealth - 10;
+            $('#hlthBr-1').attr("style", `width: ${eObj.health}%`).attr("aria-valuenow", eObj.health).text(`${eObj.health}%`)
+            //console.log(defeatedFighterObj);
+        }
+    } else{
+        fighterHealth > enemyHealth ?
+         win(): lose();
+    }
+}
+function win(){
+    alert("You Won")
+}
+function lose(){
+    alert("You Lost")
+}
+//         if ((fighterHealth > enemyHealth)) {
+//             $.extend(defeatedObj, defeatedObj);
+
+//             delete enemyObj.power;
+//             delete enemyObj.health;
+//             delete enemyObj.counter;
+//             // Test your Objects with the following code 
+//             var strA = JSON.stringify(activeObj, null, 4); // this turns the object into a string and makes it easy to read in the console.
+//             console.log("active:" + strA);
+
+//             var strB = JSON.stringify(fighterObj, null, 4);
+//             console.log("fighter:" + strB);
+
+//             var strC = JSON.stringify(enemyObj, null, 4);
+//             console.log("enemy:" + strC);
+
+//             var strD = JSON.stringify(defeatedObj, null, 4);
+//             console.log("defeated:" + strD);
+
+
+//             console.log("Fighter WIIIINNNS");
+//             $("#enemyHAlert").html("");
+//             $("#fighterHAlert").html("<strong>You won with:</strong>" + " " + fighterHealth + "%" + " left");
+//             var delayNextRound = setTimeout(function () {
+//                 $("#scoreBoard").removeClass("d-block").addClass("d-none");
+
+
+//                 nextRound();
+
+//             }, 5000);
+
+
+//         } else {
+//             console.log("Fighter LOSSES");
+//             $("#fighterHAlert").html("");
+//             $("#enemyHAlert").html("<strong>You lost with:</strong>" + " " + fighterHealth + "%" + " left");
+//             var delayEndGame = setTimeout(function () {
+//                 $("#scoreBoard").removeClass("d-block").addClass("d-none");
+//                 endGame();
+
+//             }, 5000);
+//         }
+//     }
+// }
 
 function nextRound() {
     console.log("in nextRound();");
@@ -207,7 +222,3 @@ function endGame() {
     console.log("endGame()");
 }
 
-function gameSetup() {
-    $("#attack-btn").text("Start Game");
-    startFight();
-}
